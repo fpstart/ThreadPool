8c2ba08fb10bfae561cbc8f89087994451fa6324
fpstart <123644185+fpstart@users.noreply.github.com>
2023-10-29 12:32:51 +0800

    update ThreadPool.hpp

--- ThreadPool.hpp
+++ ThreadPool.hpp
@@ -1,8 +1,14 @@
+#ifndef __ZEROREF_THREADPOOL_H__
+#define __ZEROREF_THREADPOOL_H__
+#include <chrono>
+#include <ratio>
 namespace zeroref
 {
   class ThreadPool
   {
     public:
-      ThreadPool(int coreThreadNum,int maxThreadNum,int keepAliveTime_ms)
+      ThreadPool(int coreThreadNum,int maxThreadNum,std::chrono::duration<double,std::ratio<1,1000>> keepAliveTime)
   }
 }
+#include "./ThreadPool.cpp"
+#endif
