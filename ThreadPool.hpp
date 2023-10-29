4452bf31f5fa080ee7f20c1c396249a3e6b81057
fpstart <123644185+fpstart@users.noreply.github.com>
2023-10-29 19:37:52 +0800

    update ThreadPool.hpp

--- ThreadPool.hpp
+++ ThreadPool.hpp
@@ -10,6 +10,7 @@ namespace zeroref
       ThreadPool(int coreThreadNum,int maxThreadNum,std::chrono::duration<double,std::ratio<1,1000>> keepAliveTime);
       ~ThreadPool();
       template <typename FUNC,typename... ARGS>
+      auto execute(FUNC func,ARGS... args);
   }
 }
 #include "./ThreadPool.cpp"
